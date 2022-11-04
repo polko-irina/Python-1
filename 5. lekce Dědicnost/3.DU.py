a22ec0821f7993e084f8f689dbc1c20b2d8eee29
Iryna Polko <polko-irina@seznam.cz>
2022-11-04 18:37:29 +0100

    DU.dědičnost

--- 5. lekce Dědicnost/3.DU.py
+++ 5. lekce Dědicnost/3.DU.py
@@ -20,19 +20,18 @@ class Koronavirus(Nemoc):
         return super().__str__() + f' Varianty: {", ".join(self.varianty)}.'
     
     def pridej_variantu(self, varianta):
-        for varianta in self.varianty: 
-            self.varianty.append(varianta)
+        self.varianty.append(varianta)
 
-corona = Koronavirus('Coronavirus', 10, 1000, 'vzduchem')
-#corona = Koronavirus('Coronavirus', []) # melo by fungovat stejne jako predchozi radek
-print(corona) # 'Jmeno: Coronavirus (zadne nalezene varianty)'
-print(corona.pocet_obeti) # nejake cislo ktere se da menit pomoci metody zmen_pocet_obeti() - muze byt nacatku nula nebo cislo ktere si zvolite pri vytvoreni objektu
-print(corona.sireni) # 'vzduchem' -- muzete reprezentovat i cislem
-print(corona.inkubacni_doba) # 12 -- je mi jedno jake cislo - pevne dane ve volani super().__init__(...)
-corona.pridej_varianu('omikron')
-corona.pridej_varianu('delta')
-print(corona) # 'Jmeno: Coronavirus (varianty: omikron, delta)'
-corona.pridej_varianu('alpha')
-print(corona) # 'Jmeno: Coronavirus (varianty: omikron, delta, alpha)'
+corona = Koronavirus('Coronavirus', 14, 598645, 'vzduchem')
+print(corona)
+print(corona.inkubacni_doba) 
+print(corona.pocet_obeti)
+print(corona.sireni)
+corona.pridej_variantu('omikron')
+print(corona)
+corona.pridej_variantu('delta')
+print(corona)
+corona.pridej_variantu('alpha')
+print(corona)
 
 
